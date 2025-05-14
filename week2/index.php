db6ded7e49d52571baf9721d570d860d0b6ca1d2
hetul0012 <hetulss3@gmail.com>
2025-05-14 16:37:37 -0400

    Add new code

---
+++ week2/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab week -2 
+         </title>
+</head>
+<body>
+
+<!-- Learning Echo -->
+   <?php
+        echo "Hello Wolrd!";
+
+        echo "<p>My name is Hetul Suthar.</p>";
+
+    ?>
+
+<hr>
+
+    <!-- Learning Variables -->
+   <?php
+       $fname = "Hetul";
+       $lname = "Suthar";
+
+       echo "<h1>Hello " . $fname. " " . $lname .  "</h1>";
+    ?>
+
+    <!-- learning Array -->
+    <?php
+        $people = array(
+        'Hetul',
+        'Milin',
+        'Saumil');
+        $person['first'] = 'Sam';
+        $person['last'] = 'Patel';
+?>
+
+
+</body>
+</html>
