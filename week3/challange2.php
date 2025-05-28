ebad4e937db5f35b7098587b1423e5c353aef317
hetul0012 <hetulss3@gmail.com>
2025-05-28 15:47:11 -0400

    Completed challenge 2

--- week3/challange2.php
+++ week3/challange2.php
@@ -2,6 +2,17 @@
 <?php
 
 //Challenge 2: PHP Control Structures - The Magic Number Game
+$number = rand(1,99);
+
+    if ($number % 3 === 0 && $number % 5 === 0) {
+        echo "FizzBuzz";
+    } elseif ($number % 3 === 0) {
+        echo "Fizz";
+    } elseif ($number % 5 === 0) {
+        echo "Buzz";
+    } else {
+        echo $number;
+    }
 
 
 ?>
